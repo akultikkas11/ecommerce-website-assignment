8ed0e13734248e59215721d6f34bb26ac1a44f56
akultikkas11 <akultikkas11@gmail.com>
2025-07-13 18:37:20 +0530

    Final Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary py-12">
+      <div className="container mx-auto">
+        <div className="text-white text-center flex justify-center gap-x-6">
+          <p>Home</p>
+          <p>Shop</p>
+          <p>About</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
